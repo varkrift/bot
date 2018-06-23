7cc7938f47d346cb5e5915a648470fb986f31e26
Sherzod Mamadaliev <smamadaliev@ya.ru>
2018-06-23 12:03:32 +0300

    Added new comments

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ $vk = new VK();
 
 if ($vk->data['type'] == 'message_new')
 {
-    $peer_id = $vk->data['object']['peer_id'];	// user ID
-    $text = $vk->data['object']['text'];		// incoming message
+    $peer_id = $vk->data['object']['peer_id'];  // user ID
+    $text = $vk->data['object']['text'];        // incoming message
 
-    if ($text == 'Start')
+    if ($text == 'Start') // if the text is Start, it will send a Started message.
 	{
         $vk->send($peer_id, 'Started');
     }
 	else
 	{
-    	$vk->send($peer_id, 'Error');
+    	$vk->send($peer_id, 'Error'); // error
     }
 }
