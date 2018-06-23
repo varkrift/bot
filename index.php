086b90577a9ca3a0c6b52bbec13747c79d776905
Sherzod Mamadaliev <smamadaliev@ya.ru>
2018-06-23 11:57:43 +0300

    Add new comments and fixed tabulation
    1. Add new comments user ID and incoming message.
    2. Fixed tabulation 8 to 4.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $vk = new VK();
 
 if ($vk->data['type'] == 'message_new')
 {
-    $peer_id = $vk->data['object']['peer_id'];
-    $text = $vk->data['object']['text'];
+    $peer_id = $vk->data['object']['peer_id'];	// user ID
+    $text = $vk->data['object']['text'];		// incoming message
 
     if ($text == 'Start')
 	{
