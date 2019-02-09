ec76839c4e249d3b45507c985d4ce1ba99297082
Danil Schuchkin <37618437+varkrift99@users.noreply.github.com>
2019-02-09 19:57:02 +0300

    Fixs

--- index.php
+++ index.php
@@ -7,17 +7,16 @@ if ($vk->data['type'] == 'message_new')
 {
     $peer_id = $vk->data['object']['peer_id'];  // user ID
     $text = $vk->data['object']['text'];        // incoming message
+    
+    $vk->send($peer_id, 'Здравствуйте! Я бот snowbonch 2019. Если хочешь пройти регистрацию нажми на кнопку или напиши "начать"');
 
-    if ($text == 'Start') // if the text is Start, it will send a Started message.
+    if ($text == 'Start' or $text == 'Начать' or  $text == 'начать' or $text == 'start' or $text == 'Регистрация' or $text == 'регистрация') // if the text is Hi, it will send a Hello message.
     {
-        $vk->send($peer_id, 'Started');
-    }
-    elseif ($text == 'Hi') // if the text is Hi, it will send a Hello message.
-    {
-        $vk->send($peer_id, 'Hello');
+        $vk->send($peer_id, 'Давай познакомимся поближе. Для начала укажи Ф.И.О (например: Иванов Иван Иванович) ');
+        
     }
     else
     {
-        $vk->send($peer_id, 'Error'); // error
+        $vk->send($peer_id, 'Я не понял тебя'); // error
     }
 }
